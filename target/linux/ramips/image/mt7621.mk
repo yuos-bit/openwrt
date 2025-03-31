21a740de403eac10dfbaae44c229ed06294104bf
小渔 <864529498@qq.com>
2025-03-31 20:05:40 +0800

    add e8820v2

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -726,3 +726,12 @@ define Device/zbt-wg3526-32M
 	kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
 endef
 TARGET_DEVICES += zbt-wg3526-32M
+
+define Device/e8820v2
+  DTS := E8820V2
+  IMAGE_SIZE := 16064k
+  DEVICE_TITLE := ZTE E8820V2
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 \
+	kmod-usb-ledtrig-usbport wpad-basic
+endef
+TARGET_DEVICES += e8820v2
